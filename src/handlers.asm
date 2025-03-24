1f104c1737cb6c22de88ac625aaec69571a17a60
DeshinMichael <deshinmisha26@mail.ru>
2025-03-24 11:56:25 +0700

    updated IntArbiter and main with special addres

--- src/handlers.asm
+++ src/handlers.asm
@@ -1,3 +1,9 @@
+asect 0xff7a
+uart:
+
+asect 0xfeb4
+buf: ds 30
+
 # Exception handlers section
 rsect exc_handlers
 
@@ -10,7 +16,27 @@ sample_1_int>
 	rti
 
 sample_2_int>
-	ldi r6, 7
+	ldi r0, uart
+	ldi r1, buf
+	ldi r2, 10
+
+	ldb r0, r3
+	while
+		cmp r3, r2
+	stays ne
+		stb r1, r3
+		ldb r1, r6
+		ldb r0, r3
+		inc r1
+	wend
+	clr r2
+	dec r1
+	stb r1, r2
+	ldb r1, r6
+
+	ldi r0, 0xff78
+	ldb r0, r0
+
 	rti
 
 end
--- src/main.asm
+++ src/main.asm
@@ -6,13 +6,8 @@ sample_1_int: ext
 main>
 	ldi r0, 0xff00
 	stsp r0
-
-	ldi r6, sample_1_int
 	
 	ei
-
-	ldi r0, 1
-	ldi r0, 5
 
 	w:
 	wait
