f2ba388d789747268fcf05d2d4d71b7df97b80c8
Proletcultist <zenin2606@gmail.com>
2025-04-28 16:27:31 +0700

    Add insert template errors

--- src/headers/strLiterals.h
+++ src/headers/strLiterals.h
@@ -29,3 +29,6 @@ error_large_num: ext
 error_x_higher: ext
 error_y_higher: ext
 error_many_chars: ext
+error_slot_is_empty: ext
+error_template_too_big_x: ext
+error_template_too_big_y: ext
--- src/strLiterals.asm
+++ src/strLiterals.asm
@@ -31,6 +31,9 @@ error_large_num> dc "\r\n\033[31mERROR\033[0m: Too large number!\r\n\r\n\0"
 error_x_higher> dc "\r\n\033[31mERROR\033[0m: x1 higher than x2!\r\n\r\n\0"
 error_y_higher> dc "\r\n\033[31mERROR\033[0m: y1 higher than y2!\r\n\r\n\0"
 error_many_chars> dc "\r\n\033[31mERROR\033[0m: Too many characters!\r\n\r\n\0"
+error_slot_is_empty> dc "\r\n\033[31mERROR\033[0m: Specified slot is empty!\r\n\r\n\0"
+error_template_too_big_x> dc "\r\n\033[31mERROR\033[0m: Template does not fit by width!\r\n\r\n\0"
+error_template_too_big_y> dc "\r\n\033[31mERROR\033[0m: Template does not fit by height!\r\n\r\n\0"
 
 str_help_txt> dc "\r\nAvailable commands:\r\n\r\nh - Print this help message.\r\n\r\ns1 x y - Set the cell at coordinates (x, y) to alive.\r\n\r\ns0 x y - Set the cell at coordinates (x, y) to dead.\r\n\r\ni x y - Invert the status of the cell at coordinates (x, y).\r\n\r\nplay - Start the game.\r\n\r\npause - Pause the game.\r\n\r\nstep - Perform one iteration of the game.\r\n\r\nrules S<digits>B<digits> - Set custom rules for the game. Example: rules S23B3\r\n\r\nsu - Speed up the game.\r\n\r\nsd - Slow down the game.\r\n\r\nss n - Set game speed to value n.\r\n\r\nS1 x1 y1 x2 y2 - Set all cells in the rectangle from (x1, y1) to (x2, y2) to alive.\r\n\r\nS0 x1 y1 x2 y2 - Set all cells in the rectangle from (x1, y1) to (x2, y2) to dead.\r\n\r\nsaveX n x1 y1 x2 y2 - Save all cells in the rectangle from (x1, y1) to (x2, y2) into slot number n.\r\n\r\nloadX n x y - Load cells from slot n into the field with the upper-left corner at (x, y).\r\n\r\nclr - Clear the entire game field.\r\n\0"
 
